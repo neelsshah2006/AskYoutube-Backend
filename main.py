38b36f910103b4b05cd9c580a5221917bb0d248c
neelsshah2006 <neelsshah2006@gmail.com>
2026-03-28 00:22:32 +0530

    Resolving CORS Issue

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from dotenv import load_dotenv
 load_dotenv()
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.api import router
 
-
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router)
 
 @app.get("/")
